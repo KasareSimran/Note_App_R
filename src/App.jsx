9d2bc7eefbd225d8b89de983d2856f0202b512e0
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2024-11-25 22:25:39 +0530

    Update components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Component/Header.jsx'
+import Notes from './Component/Notes.jsx';
+
+function App(){
+
+  return(
+    <div className='main'>
+    <Header/>
+    <Notes/>
+    </div>
+  );
+}
+export default App
